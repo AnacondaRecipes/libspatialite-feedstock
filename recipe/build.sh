d975d8ab66259ea96a49f29f7d84f2341ba85ff6
Michael Sarahan <msarahan@gmail.com>
2017-11-21 13:17:16 -0600

    remove .la files causing compilation trouble on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,11 @@
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 export CFLAGS="-I$PREFIX/include $CFLAGS"
 
-autoreconf -vfi
+# these files have hardcoded paths in them.  We don't need .la files anyway, so just remove it.
+if [ -f ${PREFIX}/${HOST}/lib/libstdc++.la ]; then
+    find ${PREFIX} -name "*.la" -print0 | xargs -0 rm
+fi
+
 ./configure --prefix=$PREFIX --host=$HOST --enable-static=no
 
 make
