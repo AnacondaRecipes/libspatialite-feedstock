fc7142d0cf8b413c011354ca13ccb9ff5d4c57cb
Ryan Keith <rkeith@anaconda.com>
2023-12-13 11:17:45 -0800

    First pass on updates.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,6 @@
 # make sure arm64 is a known host ...
 cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-# The `ACCEPT_USE_OF_DEPRECATED_PROJ_API_H` is a temporary solution and won't work with proj4 7.
-export CFLAGS="-I${PREFIX}/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1  ${CFLAGS}"
 
 # these files have hardcoded paths in them.  We don't need .la files anyway, so just remove it.
 if [ -f ${PREFIX}/${HOST}/lib/libstdc++.la ]; then
