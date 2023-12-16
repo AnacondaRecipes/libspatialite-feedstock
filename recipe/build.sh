1e1f1b8374c52c10be74af3582744c6b8471f5e1
Ryan Keith <rkeith@anaconda.com>
2023-12-15 17:51:42 -0800

    Adjusting makefiles.

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,16 +9,29 @@ if [ -f ${PREFIX}/${HOST}/lib/libstdc++.la ]; then
     find ${PREFIX} -name "*.la" -print0 | xargs -0 rm
 fi
 
+# Disabling geos 3.10 and 3.11 as we are building against geos 3.9
+# Disabling rttopo as currently not available in main channel
+# Disabling minizip to reduce dependencies (could enabled)
+
 if [[ $(uname -m) == "aarch64" ]]; then
   ./configure --prefix=${PREFIX} \
               --host=aarch64-linux-gnu \
               --build=aarch64-linux-gnu \
-              --enable-static=no
+              --enable-static=no \
+              --enable-geos3100=no \
+              --enable-geos3110=no \
+              --enable-rttopo=no \
+              --enable-minizip=no
+
 else
   ./configure --prefix=${PREFIX} \
               --host=${HOST} \
               --build=${BUILD} \
-              --enable-static=no
+              --enable-static=no \
+              --enable-geos3100=no \
+              --enable-geos3110=no \
+              --enable-rttopo=no \
+              --enable-minizip=no
 fi
 
 make
